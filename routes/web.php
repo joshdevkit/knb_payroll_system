74995d6808ef08ad69d3fc9413a44f947d0e8510
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2026-09-10 07:50:45 +0800

    Use resource route for payroll register

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PayrollRunController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
@@ -14,4 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('employees', EmployeeController::class);
     Route::resource('categories', CategoryController::class);
+    Route::resource('payroll-register', PayrollRunController::class)
+        ->except(['create', 'edit']);
 });

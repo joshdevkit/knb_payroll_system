a626c78077fabe2dfae4deb6b79acfed7aef0996
Joshua Pacho <jshpch1996@gmail.com>
2026-09-10 07:09:23 +0800

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/',[AuthController::class, 'index'])->name('login');
